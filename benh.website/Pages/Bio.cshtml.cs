70bfdf5801cb0970974e6325df5f597bef40e3b4
Ben Holland <bhollan5@kent.edu>
2021-07-23 22:32:49 -0600

    First commit! The website already has a few pages.

---
+++ benh.website/Pages/Bio.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace benh.website.Pages
+{
+    public class BioModel : PageModel
+    {
+        private readonly ILogger<BioModel> _logger;
+
+        public BioModel(ILogger<BioModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
