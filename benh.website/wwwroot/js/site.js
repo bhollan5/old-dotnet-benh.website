7cea8b93f3d9bd991dd15ab1d7b48793f41b5975
Ben Holland <bhollan5@kent.edu>
2021-07-25 12:29:51 -0400

    Links are now highlighted correctly.

--- benh.website/wwwroot/js/site.js
+++ benh.website/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+function onload() {
+
+}
+
+function check_navbar() {
+
+    
+    var current_route = window.location.href;
+
+    console.log(current_route);
+    var bio_link = document.getElementById("bio-link");
+    
+}
