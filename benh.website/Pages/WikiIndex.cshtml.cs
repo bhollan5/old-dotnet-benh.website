c492bd1bc78a07ab0531f39e9626c7ea17204ca9
Ben Holland <bhollan5@kent.edu>
2021-08-06 14:28:28 -0400

    Adding hexaguessimal files

---
+++ benh.website/Pages/WikiIndex.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace benh.website.Pages
+{
+    public class WikiIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
